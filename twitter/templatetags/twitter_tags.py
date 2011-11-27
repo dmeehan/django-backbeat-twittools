fb73b6deab76b236f729edbf06e68f3c77f40531
Douglas Meehan <dmeehan@gmail.com>
2011-11-27 15:16:02 -0500

    converted indents to spaces

--- twitter/templatetags/twitter_tags.py
+++ twitter/templatetags/twitter_tags.py
@@ -8,22 +8,22 @@ register = template.Library()
 @register.tag(name="get_friends_timeline")
 def do_get_friends_timeline(parser, token):
     """
-	Call this tag with:
-		get_friends_timeline as <context_object>
-	"""
+    Call this tag with:
+        get_friends_timeline as <context_object>
+    """
     bits = token.split_contents()
-	if len(bits) != 3:
-	    raise TemplateSyntaxError, "%s takes 2 arguments" % bits[0]
-	if bits[1] != "as":
-		raise TemplateSyntaxError, "First argument for %s should be 'as'" % bits[0]
-	return FriendsTimelineNode(bits[2])
-    
+    if len(bits) != 3:
+        raise TemplateSyntaxError, "%s takes 2 arguments" % bits[0]
+    if bits[1] != "as":
+        raise TemplateSyntaxError, "First argument for %s should be 'as'" % bits[0]
+    return FriendsTimelineNode(bits[2])
+
 class FriendsTimelineNode(node):
     def __init__(self, context_object):
         self.context_object = context_object
     def render(self, context):
         try:
-			api = twitter.Api(consumer_key=settings.TWITTER_CONSUMER_KEY,
+            api = twitter.Api(consumer_key=settings.TWITTER_CONSUMER_KEY,
                               consumer_secret=settings.TWITTER_CONSUMER_SECRET,
                               access_token_key=settings.TWITTER_ACCESS_TOKEN_KEY,
                               access_token_secret=settings.TWITTER_ACCESS_TOKEN_SECRET)
@@ -32,19 +32,19 @@ class FriendsTimelineNode(node):
                 "name": "%s" % timeline.user.name,
                 "screen_name": "%s" % timeline.user.screen_name,
                 "avatar": "%s" % timeline.user.profile_image_url,
-				"text": "%s" % timeline.text,
-				"time": "%s" % timeline.relative_created_at,
+                "text": "%s" % timeline.text,
+                "time": "%s" % timeline.relative_created_at,
                 "avatar": "%s" % timeline.user.profile_image_url,
-			}
+            }
 
-		except:
+        except:
             context[self.context_object] = {
-				"name": "Error",
-				"screen_name": "",
+                "name": "Error",
+                "screen_name": "",
                 "avatar": "",
-				"status": "",
-				"time": "",
+                "status": "",
+                "time": "",
                 "avatar": "",
-			}
+            }
 
-		return ''
+        return ''
