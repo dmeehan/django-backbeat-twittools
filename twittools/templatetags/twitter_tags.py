378b6ee2d261f26762f2679a065e91d02c75d6af
Douglas Meehan <dmeehan@gmail.com>
2012-02-02 00:30:24 -0500

    adding error handling

--- twittools/templatetags/twitter_tags.py
+++ twittools/templatetags/twitter_tags.py
@@ -42,13 +42,14 @@ class FriendsTimelineNode(Node):
     def __init__(self, context_object):
         self.context_object = context_object
     def render(self, context):
+
         try:
             api = twitter.Api(consumer_key='%s' % settings.TWITTER_CONSUMER_KEY,
                               consumer_secret='%s' % settings.TWITTER_CONSUMER_SECRET,
                               access_token_key= '%s' % settings.TWITTER_ACCESS_TOKEN_KEY,
                               access_token_secret='%s' % settings.TWITTER_ACCESS_TOKEN_SECRET)
-            timeline = api.GetFriendsTimeline()
-            context[self.context_object] = timeline
+            #timeline = api.GetFriendsTimeline()
+            #context[self.context_object] = timeline
 
         except:
             raise ImproperlyConfigured("Could not access Twitter account.")
